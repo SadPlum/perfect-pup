87de2a29d1871d053823ff21ceab76b21541b383
SadPlum <92168542+SadPlum@users.noreply.github.com>
2022-03-20 20:55:59 -0300

    Made footer and finished about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   BrowserRouter as BrowserRouter,
   Routes,
@@ -8,20 +7,27 @@ import {
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/random"></Route>
-        <Route path="/search"></Route>
-        <Route path="/contact"></Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/tips"></Route>
-      </Routes>
+      <div className="container">
+        <Header />
+        <div className="wrap">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/random"></Route>
+            <Route path="/search"></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/tips"></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2 className="contact-title">Where you can find us</h2>
+    </section>
+  );
+}
+
+export default Contact;
