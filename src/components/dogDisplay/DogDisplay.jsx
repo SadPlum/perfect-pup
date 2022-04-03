3f2b475abd3338afb0587bfa0c7f2cab490eec3c
SadPlum <92168542+SadPlum@users.noreply.github.com>
2022-04-02 18:54:59 -0700

    Updating all changes to use on PC

--- src/components/dogDisplay/DogDisplay.jsx
+++ src/components/dogDisplay/DogDisplay.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import pawIcon from "../../icons/paw.png";
+import { Link } from "react-router-dom";
+import SingleDog from "../singleDog/SingleDog";
 
-function dogDisplay({ url, name, sex }) {
+function DogDisplay({ name, sex, id, breed, age, temperament, img }) {
   return (
-    <article className="dogDisplay">
-      <div className="dogDisplay-head">
-        <h3 className="dogDisplay-title">{`${name}, ${sex}`}</h3>
-      </div>
-      <div className="dogDisplay-content">
-        <img src={url} alt="" className="dogDisplay-img" />
-        <button>{`Learn about ${name}`}</button>
-      </div>
-    </article>
+    <>
+      <article className="dogDisplay">
+        <div className="dogDisplay-head">
+          <h3 className="dogDisplay-title">{`${name}, ${sex}`}</h3>
+        </div>
+        <div className="dogDisplay-content">
+          <img src={img} alt="" className="dogDisplay-img" />
+          <button className="dogDisplay-learn-btn">{`Learn about ${name}`}</button>
+        </div>
+        <img src={pawIcon} alt="paw icon" className="dogDisplay-paw-icon" />
+      </article>
+    </>
   );
 }
 
-export default dogDisplay;
+export default DogDisplay;
--- src/components/dogSeach/DogSearch.jsx
+++ src/components/dogSeach/DogSearch.jsx
@@ -3,19 +3,36 @@ import SingleDog from "../singleDog/SingleDog";
 import DogDisplay from "../dogDisplay/DogDisplay";
 
 function DogSearch({ dogList }) {
+  const [sexValue, setSexValue] = useState("");
+  const [dogArr, setDogArr] = useState(dogList);
+
+  // SETS FILTER TO SEARCH DOG BY SEX
+  const handleChange = (value) => {
+    setSexValue(value);
+    value !== ""
+      ? setDogArr(dogList.filter((dog) => dog.dog.sex === value))
+      : setDogArr(dogList);
+  };
+
   return (
     <>
       <section className="dogSearch">
-        <h2 className="dogSearch-title"> List of available dogs</h2>
-        <label for="dogBreeds">Breeds:</label>
+        <div className="dogSearch-head">
+          <h2 className="dogSearch-title"> List of available dogs</h2>
+          {/* <label for="dogBreeds">Breeds:</label>
         <select name="dogBreeds" id="dogBreeds">
           <option>Will have breeds</option>
-        </select>
-        <label for="dogSex">Sex:</label>
-        <select name="dogSex" id="dogSex">
-          <option>Male</option>
-          <option>Female</option>
-        </select>
+        </select> */}
+          <label>Sex:</label>
+          <select
+            value={sexValue}
+            onChange={(e) => handleChange(e.target.value)}
+          >
+            <option value="">Both</option>
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+          </select>
+        </div>
         {/* 
         {dogList &&
           dogList.map((dog, i) => (
@@ -26,14 +43,24 @@ function DogSearch({ dogList }) {
               breed={dog.breeds[0].name}
             />
           ))} */}
-        {dogList &&
-          dogList.map((dog, i) => {
-            let gender;
-            console.log(i);
-            if (i === 0 || i % 2 === 0) gender = "M";
-            else gender = "F";
-            return <DogDisplay key={i} url={dog.url} sex={gender} />;
-          })}
+
+        {/* IF DOG IS NOT ADOPTED, RETURN PROFILES */}
+        {dogArr.map((dog, i) => {
+          if (dog.adopted === false) {
+            return (
+              <DogDisplay
+                key={i}
+                id={dog.i}
+                name={dog.dog.name}
+                breed={dog.dog.breed}
+                sex={dog.dog.sex}
+                img={dog.dog.img}
+                age={dog.dog.age}
+                temperament={dog.dog.temperament}
+              />
+            );
+          }
+        })}
       </section>
     </>
   );
